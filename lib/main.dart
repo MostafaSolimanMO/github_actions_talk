5f8abd51c721a15d90eb148675339c80d7349bed
Mostafa Soliman <mostafamoma.moma@gmail.com>
2022-03-25 14:14:34 +0200

    feat:change color

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.purple,
+        primarySwatch: Colors.red,
       ),
       home: const MyHomePage(
         title: 'Flutter Festival ❤️',
